d5491acc1dde0135be06fedfecb0725545fcb9b2
jpramirez <pabloelcolombiano@gmail.com>
2020-04-23 01:55:03 +0200

    Task #3287
    * Old tests and TesApp removed

--- tests/EntitiesTestApp/src/Model/Entity/Address.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace TestApp\Model\Entity;
-
-use Cake\ORM\Entity as BaseEntity;
-
-class Address extends BaseEntity
-{
-}
--- tests/EntitiesTestApp/src/Model/Entity/Project.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace TestApp\Model\Entity;
-
-use Cake\ORM\Entity;
-
-class Project extends Entity
-{
-}
