8dbd3600195c8e43626711c731aa820b58970425
jpramirez <pabloelcolombiano@gmail.com>
2020-06-05 14:27:10 +0200

    Licenses added

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,12 +1,14 @@
 <?php
+declare(strict_types=1);
+
 /**
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
  * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
+ * @copyright     Copyright (c) 2020 Juan Pablo Ramirez and Nicolas Masson
+ * @link          https://webrider.de/
+ * @since         1.0.0
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 namespace CakephpFixtureFactories;
