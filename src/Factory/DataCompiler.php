7942c1d9c39ed0a104b0874439514871aef3197e
Jesper Schmitz Mouridsen <jesper@schmitz.computer>
2021-04-15 11:49:11 +0200

    Add missing condional

--- src/Factory/DataCompiler.php
+++ src/Factory/DataCompiler.php
@@ -498,9 +498,11 @@ class DataCompiler
             foreach ($primaryKeys as $pk => $offset) {
                 $seq = $this->getFactory()->getRootTableRegistry()->getConnection()->execute("
 		            SELECT pg_get_serial_sequence('$tableName','$pk')")->fetchAll()[0][0];
+		if ($seq !== NULL) {
                     $this->getFactory()->getRootTableRegistry()->getConnection()->execute(
                         "SELECT setval('$seq', $offset);"
                 );
+		}
             }
         }
     }
