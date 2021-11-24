a88b081c51b1f4d6a60d154d0e037c9a4a628596
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2021-11-24 10:38:21 +0100

    #150 Illustrates associations with factories outside of the default directory.

---
+++ tests/Factory/SubDirectory/SubCityFactory.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) 2020 Juan Pablo Ramirez and Nicolas Masson
+ * @link          https://webrider.de/
+ * @since         1.0.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace CakephpFixtureFactories\Test\Factory\SubDirectory;
+
+use CakephpFixtureFactories\Test\Factory\CityFactory as BaseFactory;
+
+/**
+ * Class CityFactory
+ * This factory is used to test factories in other namespaces
+ */
+class SubCityFactory extends BaseFactory
+{
+}
